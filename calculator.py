e806e869c675b6162d7525e7e360246245b4a4a1
michalleonardi22 <leonardimichael22@gmail.com>
2023-12-04 17:37:19 +0700

    memasukkan fungsi tambah

--- calculator.py
+++ calculator.py
@@ -1,7 +1,10 @@
+def penjumlahan(a,b):
+  return a + b
 def main():
   first_insert = input("masukkan angka pertama: ")
   second_insert = input("masukkan angka kedua: ")
   print(first_insert)
   print(second_insert)
+  print("penjumlahan = " + str(first_insert) + " + " + str(second_insert) + " = " + str(penjumlahan(int(first_insert),int(second_insert))))
 if __name__ == "__main__":
   main()
