e27958bb64d54fd560562940858f76e94185cd27
Leo <57362008+michaelleonardi22@users.noreply.github.com>
2023-12-04 17:26:40 +0700

    menu awal

---
+++ calculator.py
@@ -0,0 +1,7 @@
+def main():
+  first_insert = input("masukkan angka pertama: ")
+  second_insert = input("masukkan angka kedua: ")
+  print(first_insert)
+  print(second_insert)
+if __name__ == "__main__":
+  main()
