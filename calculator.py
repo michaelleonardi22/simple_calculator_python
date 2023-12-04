8d066b2286cbc3002ee2c06c24d5e94f8be28633
witaradya <rajaradya07@gmail.com>
2023-12-04 17:45:40 +0700

    Program pembagian dan perkalian

--- calculator.py
+++ calculator.py
@@ -1,7 +1,20 @@
+def perkalian(angka1, angka2):
+  return angka1 * angka2
+
+def pembagian(angka1, angka2):
+  return angka1 / angka2
+
 def main():
   first_insert = input("masukkan angka pertama: ")
   second_insert = input("masukkan angka kedua: ")
   print(first_insert)
   print(second_insert)
+  
+  hasil =  perkalian(int(first_insert), int(second_insert))
+  print(f'Hasil Perkalian dari {first_insert} dan {second_insert} = {hasil}')
+
+  hasil =  pembagian(int(first_insert), int(second_insert))
+  print(f'Hasil Pembagian dari {first_insert} dan {second_insert} = {hasil:.2f}')
+
 if __name__ == "__main__":
   main()
